a89ee702176a29385c26a33263f5085a8d932e41
M. Hosseyn Najafi <hosseynjf@icloud.com>
2020-05-09 13:37:01 +0430

    Add callback register system to events

--- openvpn_api/events/__init__.py
+++ openvpn_api/events/__init__.py
@@ -5,6 +5,13 @@ from openvpn_api.events import client
 
 event_types = [importlib.import_module(".client", __name__)]
 
+_callbacks = []
+
 
 def raise_event(event: typing.Type) -> None:
-    raise NotImplementedError
+    for callback in _callbacks:
+        callback(event)
+
+
+def register_callback(callback: typing.Callable[[typing.Type], typing.Any]) -> None:
+    _callbacks.append(callback)
